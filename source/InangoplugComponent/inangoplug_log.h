bf8033b743d7b90b6a859a2a41a1cd7742e6ac20
inango-integrator <39555614+inango-integrator@users.noreply.github.com>
2022-03-31 17:03:53 +0500

    Release from 31/03/2022

--- source/InangoplugComponent/inangoplug_log.h
+++ source/InangoplugComponent/inangoplug_log.h
@@ -1,14 +1,13 @@
 #include "rdk_debug.h"
 
+#define DEBUG_INI_NAME  "/etc/debug.ini"
+
+#define inangoplug_log_init()    rdk_logger_init(DEBUG_INI_NAME)
+
 /**
  * @brief Configures different log level CcspInangoplugComponent
  */
-#define inangoplug_log_error(...)       inangoplug_log(RDK_LOG_ERROR, __VA_ARGS__)
-#define inangoplug_log_info(...)        inangoplug_log(RDK_LOG_INFO, __VA_ARGS__)
-#define inangoplug_log_warning(...)     inangoplug_log(RDK_LOG_WARN, __VA_ARGS__)
-#define inangoplug_log_debug(...)       inangoplug_log(RDK_LOG_DEBUG, __VA_ARGS__)
-
-void inangoplug_log(unsigned int level, const char *msg, ...)
-    __attribute__((format (printf, 2, 3)));
-
-void inangoplug_log_init();
+#define inangoplug_log_error(format, ...)       RDK_LOG(RDK_LOG_ERROR, "LOG.RDK.INANGOPLUG", format, ##__VA_ARGS__)
+#define inangoplug_log_info(format, ...)        RDK_LOG(RDK_LOG_INFO, "LOG.RDK.INANGOPLUG", format, ##__VA_ARGS__)
+#define inangoplug_log_warning(format, ...)     RDK_LOG(RDK_LOG_WARN, "LOG.RDK.INANGOPLUG", format, ##__VA_ARGS__)
+#define inangoplug_log_debug(format, ...)       RDK_LOG(RDK_LOG_DEBUG, "LOG.RDK.INANGOPLUG", format, ##__VA_ARGS__)
